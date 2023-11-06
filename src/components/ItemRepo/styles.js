b45f075b9ea796fd72c062194435ba73a63a4b88
samuel henrique <samuellimaarruda@hotmail.com>
2023-11-06 17:06:58 -0400

    Projeto finalizado

--- src/components/ItemRepo/styles.js
+++ src/components/ItemRepo/styles.js
@@ -1,26 +1,29 @@
 import styled from "styled-components";
 
 export const ItemContainer = styled.div`
-    width: 80%;
+  width: 80%;
 
-    h3 {
-        font-size: 32px;
-        color: #FAFAFA;
-    }
+  h3 {
+    font-size: 32px;
+    color: #fafafa;
+  }
 
-    p {
-        font-size:16px;
-        color: #FAFAFA60;
-        margin-bottom:20px;
-    }
+  p {
+    font-size: 16px;
+    color: #fafafa60;
+    margin-bottom: 20px;
+  }
 
-    a.remover {
-        color: #FF0000;
-        margin-top:20px;
-    }
+  .remover {
+    background-color: transparent;
+    color: #ff0000;
+    margin-top: 20px;
+    border: 0;
+    cursor: pointer;
+  }
 
-    hr {
-        color: #FAFAFA60;
-        margin: 20px 0;
-    }
-`
+  hr {
+    color: #fafafa60;
+    margin: 20px 0;
+  }
+`;
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,51 +1,52 @@
+import { useState } from "react";
+import gitLogo from "../assets/github.png";
+import Input from "../components/Input";
+import Button from "../components/Button";
+import ItemRepo from "../components/ItemRepo";
+import { api } from "../services/api";
 
-import { useState } from 'react';
-import gitLogo from '../assets/github.png'
-import Input from '../components/Input';
-import Button from '../components/Button';
-import ItemRepo from '../components/ItemRepo';
-import { api } from '../services/api';
-
-import { Container } from './styles';
+import { Container } from "./styles";
 
 function App() {
-
-  const [currentRepo, setCurrentRepo] = useState('');
+  const [currentRepo, setCurrentRepo] = useState("");
   const [repos, setRepos] = useState([]);
 
-
   const handleSearchRepo = async () => {
+    try {
+      const { data } = await api.get(`repos/${currentRepo}`);
 
-    const {data} = await api.get(`repos/${currentRepo}`)
+      if (data.id) {
+        const isExist = repos.find((repo) => repo.id === data.id);
 
-    if(data.id){
-
-      const isExist = repos.find(repo => repo.id === data.id);
-
-      if(!isExist){
-        setRepos(prev => [...prev, data]);
-        setCurrentRepo('')
-        return
+        if (!isExist) {
+          setRepos((prev) => [...prev, data]);
+          setCurrentRepo("");
+          return;
+        } else {
+          alert("Repositório já adicionado!");
+        }
       }
-
+    } catch (error) {
+      alert("Repositório não encontrado");
     }
-    alert('Repositório não encontrado')
-
-  }
+  };
 
   const handleRemoveRepo = (id) => {
-    console.log('Removendo registro', id);
-
-    // utilizar filter.
-  }
-
+    const newRepos = repos.filter((repo) => repo.id !== id);
+    setRepos(newRepos);
+  };
 
   return (
     <Container>
-      <img src={gitLogo} width={72} height={72} alt="github logo"/>
-      <Input value={currentRepo} onChange={(e) => setCurrentRepo(e.target.value)} />
-      <Button onClick={handleSearchRepo}/>
-      {repos.map(repo => <ItemRepo handleRemoveRepo={handleRemoveRepo} repo={repo}/>)}
+      <img src={gitLogo} width={72} height={72} alt="github logo" />
+      <Input
+        value={currentRepo}
+        onChange={(e) => setCurrentRepo(e.target.value)}
+      />
+      <Button onClick={handleSearchRepo} />
+      {repos.map((repo) => (
+        <ItemRepo handleRemoveRepo={handleRemoveRepo} repo={repo} />
+      ))}
     </Container>
   );
 }
